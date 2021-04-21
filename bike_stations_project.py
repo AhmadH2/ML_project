5aee7b34fc8fca36bb65e7b31deac69f99324a91
Ahmad Horyzat <171059@ppu.edu.ps>
2021-04-21 15:34:00 +0300

    first commit

---
+++ bike_stations_project.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+ML PROJECT
+@authors: 
+    Ahmad Horyzat
+    Mohammad Saleh
+    Yousef Abu Rayan
+"""
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('202009-capitalbikeshare-tripdata.csv')
+
+print(df.columns)
